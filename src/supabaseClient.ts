ea92160a1de62d77132c0a71b5edce57479e002a
deepcrown-moon <deepcrown1956@gmail.com>
2026-09-12 00:20:28 +0700

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://hklvpebvphfvremlpyqv.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://hklvpebvphfvremlpyqv.supabase.co'
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_Onq6OZSH6SvyWiON3z9C3w_txPcR5SN'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY)
